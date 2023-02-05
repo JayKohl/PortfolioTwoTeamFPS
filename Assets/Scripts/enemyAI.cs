a11949fcada7ed9e7a546031c9a5633aa20b66e0
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-05 16:28:55 -0500

    enemyAI set up
    Added most core variables needed for the enemy AI.

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// getting access to the nav mesh.
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour
 {
+    [Header("----- Components -----")]
+    // For flashing the material red "visual feedback"
+    [SerializeField] Renderer model;
+    // This is to attach the enemy to the nav mesh. 
+    [SerializeField] NavMeshAgent agent;
+
+    [Header("----- Enemy Stats -----")]
+    [SerializeField] int hitPoints;
+    [SerializeField] int playerFaceSpeed;
+
+    [Header("----- Gun -----")]
+    [SerializeField] Transform shootPosition;
+    [SerializeField] GameObject bullet;
+    [SerializeField] int bulletSpeed;
+    [SerializeField] float shootRate;
+
+    Vector3 playerDirection;
+    bool isPlayerInRange;
+    bool isShooting;
+
     // Start is called before the first frame update
     void Start()
     {
