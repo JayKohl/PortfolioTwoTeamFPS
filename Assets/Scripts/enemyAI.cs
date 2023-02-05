3024ea5bf1e180ca333b09fe8d59cce8b1b57076
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-05 17:12:07 -0500

    Update enemyAI.cs
    Updating the game goal in start when an enemy instance is created.

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -29,7 +29,7 @@ public class enemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager.instance.updateGameGoal(1);
     }
 
     // Update is called once per frame
