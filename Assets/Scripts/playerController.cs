0d49e8575694eb07926659be80a09db1fa23d9a0
Keith Laurie <KTLaurie@student.fullsail.edu>
2023-02-06 17:53:43 -0500

    Added members to playerController

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [Header("----- Components -----")]
+    [SerializeField] CharacterController controller;
+
+    [Header("----- Player Stats -----")]
+    [Range(5, 10)] [SerializeField] int HP;
+    [Range(1, 5)] [SerializeField] int playerSpeed;
+    [Range(1, 3)] [SerializeField] int jumpTimes;
+    [Range(10, 25)] [SerializeField] int jumpSpeed;
+    [Range(15, 45)] [SerializeField] int gravity;
+
+    [Header("----- Gun Stats -----")]
+    [SerializeField] float shootRate;
+    [SerializeField] int shootDist;
+    [SerializeField] int shootDamage;
+    [SerializeField] GameObject cube;
+
+    int jumpsCurrent;
+    Vector3 move;
+    Vector3 playerVelocity;
+    bool isShooting;
+    int HPOriginal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HPOriginal = HP;
+        updatePlayerHPBar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+        if (!isShooting && Input.GetButton("Shoot"))
+            StartCoroutine(shoot());
     }
+
 }
