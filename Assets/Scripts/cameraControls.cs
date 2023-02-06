d46fcc0943cecbaa1aa8fc4af34710b15b5fc2a7
Teirons <TMIRONS@STUDENT.FULLSAIL.EDU>
2023-02-06 18:21:05 -0500

    CameraControls
    Added code for camera movement.

--- Assets/Scripts/cameraControls.cs
+++ Assets/Scripts/cameraControls.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class cameraControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] bool invertX;
+
+    [SerializeField] int HorA;
+    [SerializeField] int VerA;
+    [SerializeField] int VerMin;
+    [SerializeField] int VerMax;
+
+    float xRotation;
+
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensVer;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensHor;
+
+        if (invertX)
+            xRotation += mouseY;
+        else
+            xRotation -= mouseY;
+       
+        xRotation = Mathf.Clamp(xRotation, VerMin, VerMax);
+
+        //rotate on X-axis
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        //rotate on y-axis
+        transform.parent.Rotate(Vector3.up * mouseX);
     }
 }
