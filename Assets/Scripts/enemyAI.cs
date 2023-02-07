124f3aa1d9f0422703180271da7434eca9e800df
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-07 15:37:42 -0500

    Update enemyAI.cs
    Removed the game goal update from start.

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -32,7 +32,7 @@ public class enemyAI : MonoBehaviour, IDamage
     // Start is called before the first frame update
     void Start()
     {
-        gameManager.instance.updateGameGoal(1);
+
     }
 
     // Update is called once per frame
