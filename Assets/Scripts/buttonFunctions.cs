8d8f9ca60b579372b4b3a3c388149d6ab5691fed
OznarG <103035057+OznarG@users.noreply.github.com>
2023-02-06 19:11:15 -0500

    Button Funtion
    Button Function script was added

--- Assets/Scripts/buttonFunctions.cs
+++ Assets/Scripts/buttonFunctions.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void resume()
     {
-        
+        gameManager.instance.unPause();
+        gameManager.instance.isPaused = !gameManager.instance.isPaused;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void restart()
     {
-        
+        gameManager.instance.unPause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
     }
 }
