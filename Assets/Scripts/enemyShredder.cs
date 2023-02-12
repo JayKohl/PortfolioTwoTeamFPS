4cfe8e351b8c31ae486f44c7d8695daab36bdda5
JayKohl <jaymkohl@gmail.com>
2023-02-12 12:43:32 -0500

    New Enemy
    Working on an enemy that spawns and aggressively tracks the player... Melee damage.

---
+++ Assets/Scripts/enemyShredder.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShredder : enemyAI
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+        stoppingDistOrig = agent.stoppingDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = gameManager.instance.player.transform.position;
+    }
+    public override IEnumerator shoot()
+    {
+        isShooting = true;
+
+        float distanceForMelee = Vector3.Distance(gameManager.instance.player.transform.position, transform.position);
+        if (distanceForMelee < 2)
+        {
+            gameManager.instance.playerScript.takeDamage(1);
+        }
+
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+}
