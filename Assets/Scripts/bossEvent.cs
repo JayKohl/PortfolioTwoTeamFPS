b2b87feb16c5fa957dcc22a9cc8b912e3473863f
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-13 21:13:49 -0500

    boss Event script update
    The script for the boss event is now working. Lasers are removed prior to the player entering the boss area. Lasers then appear and a collider is activated so that the player cannot leave until the boss is dead. Once the boss dies. Lasers disappear once more and the collider is deactivated.

--- Assets/Scripts/bossEvent.cs
+++ Assets/Scripts/bossEvent.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class bossEvent : MonoBehaviour
 {
-    public bool enterBossArea;
+    public bool bossKill;
+    [SerializeField] GameObject bossAreaEntrance;
+    [SerializeField] BoxCollider toggleMesh;
+    [SerializeField] GameObject bossAttachedToDoor;
+    [SerializeField] Renderer setLazerColor;
+    [SerializeField] Renderer setLazerColorTwo;
+    [SerializeField] Renderer setLazerColorThree;
+    [SerializeField] Renderer setLazerColorFour;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bossKill = false;
+        setLazerColor.material.color = Color.clear;
+        setLazerColorTwo.material.color = Color.clear;
+        setLazerColorThree.material.color = Color.clear;
+        setLazerColorFour.material.color = Color.clear;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (bossAttachedToDoor == null)
+        {
+            setLazerColor.material.color = Color.clear;
+            setLazerColorTwo.material.color = Color.clear;
+            setLazerColorThree.material.color = Color.clear;
+            setLazerColorFour.material.color = Color.clear;
+            bossKill = true;
+            toggleMesh.enabled = false;
+        }
+    }
+    IEnumerator toggleDoorLock()
+    {
+        yield return new WaitForSeconds(.15f);
+        toggleMesh.enabled = true;//toggleMesh;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!bossKill)
+        {
+            StartCoroutine(toggleDoorLock());
+            setLazerColor.material.color = Color.white;
+            setLazerColorTwo.material.color = Color.white;
+            setLazerColorThree.material.color = Color.white;
+            setLazerColorFour.material.color = Color.white;
+        }
     }
 }
