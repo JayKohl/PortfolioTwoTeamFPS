c07aae61f9d8fa23317effde887fa44105ad1b51
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-15 17:40:01 -0500

    Boss entrance bug fix.
    Turning off the player controller and forcing the player position forward to ensure the door activates and does not lock out the player.

--- Assets/Scripts/bossEvent.cs
+++ Assets/Scripts/bossEvent.cs
@@ -12,6 +12,7 @@ public class bossEvent : MonoBehaviour
     [SerializeField] Renderer setLazerColorTwo;
     [SerializeField] Renderer setLazerColorThree;
     [SerializeField] Renderer setLazerColorFour;
+    [SerializeField] GameObject preventRunning;
 
     // Start is called before the first frame update
     void Start()
@@ -39,13 +40,16 @@ public class bossEvent : MonoBehaviour
     {
         yield return new WaitForSeconds(.15f);
         toggleMesh.enabled = true;//toggleMesh;
+        gameManager.instance.playerScript.controller.enabled = true;
     }
     private void OnTriggerEnter(Collider other)
     {
         if (!bossKill)
         {
             gameManager.instance.playerScript.controller.enabled = false;
+            gameManager.instance.player.transform.position = preventRunning.transform.position;
             StartCoroutine(toggleDoorLock());
+
             setLazerColor.material.color = Color.white;
             setLazerColorTwo.material.color = Color.white;
             setLazerColorThree.material.color = Color.white;
