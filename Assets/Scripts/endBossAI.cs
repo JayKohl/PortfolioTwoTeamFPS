4978e539b5730e5e223b7b868adff5b3a9f6c8f1
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-19 12:01:50 -0500

    Update endBossAI.cs
    Preparing the script for boss events.

--- Assets/Scripts/endBossAI.cs
+++ Assets/Scripts/endBossAI.cs
@@ -7,12 +7,46 @@ public class endBossAI : enemyShredder
     //// Start is called before the first frame update
     //void Start()
     //{
-        
+
     //}
 
     //// Update is called once per frame
     //void Update()
     //{
-        
+
     //}
+    protected override bool canSeePlayer()
+    {
+        playerDirection = (gameManager.instance.player.transform.position - headPos.position).normalized;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDirection.x, 0, playerDirection.z), transform.forward);
+
+        Debug.Log(angleToPlayer);
+        Debug.DrawRay(headPos.position, playerDirection);
+
+        Vector3 two = agent.transform.position;
+        Vector3 one = gameManager.instance.player.transform.position;
+        float distanceToEnemy = Mathf.Sqrt(Mathf.Pow((two.x - one.x), 2) + Mathf.Pow((two.y - one.y), 2) + Mathf.Pow((two.z - one.z), 2));
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDirection, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewAngle)
+            {
+                agent.stoppingDistance = stoppingDistOrig;
+                agent.speed = speedChase;
+                agent.SetDestination(gameManager.instance.player.transform.position);
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    facePlayer();
+                }
+                if (!isMelee && angleToPlayer <= shootAngle && distanceToEnemy <= 2)
+                {
+                    StartCoroutine(melee());
+                }
+                return true;
+            }
+        }
+        agent.stoppingDistance = 0;
+        return false;
+    }
 }
