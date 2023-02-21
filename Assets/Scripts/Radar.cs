c52e43280578f8a3f4ef90a3293e67c0de2db320
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2023-02-21 09:55:57 -0500

    Radar Funtional

--- Assets/Scripts/Radar.cs
+++ Assets/Scripts/Radar.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Radar : MonoBehaviour
 {
+    [SerializeField] Transform radar;
+    float radarSpeed;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+       
+        radarSpeed = 180f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        radar.eulerAngles -= new Vector3(0, 0, radarSpeed * Time.deltaTime);
     }
 }
