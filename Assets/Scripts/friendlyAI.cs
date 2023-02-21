7b4d68d83fdbfcb57c813b47d3c9ff6b4e4f8f8c
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-20 21:49:17 -0500

    Update friendlyAI.cs
    created a method that tells the the NPC how to act when they see the player.

--- Assets/Scripts/friendlyAI.cs
+++ Assets/Scripts/friendlyAI.cs
@@ -16,18 +16,24 @@ public class friendlyAI : MonoBehaviour
     [SerializeField] int viewAngle;
     [SerializeField] int waitTime;
     [SerializeField] int roamDist;
+    [SerializeField] int speedFast;
 
     bool isPlayerInRange;
+    bool isTalking;
     float angleToPlayer;
+    Vector3 playerDirection;
     Vector3 startingPos;
     Vector3 moveToTerminal;
     bool destinationChosen;
+    float stoppingDistOrig;
+    float speedOrig;
 
     // Start is called before the first frame update
     void Start()
     {
         startingPos = transform.position;
-        
+        stoppingDistOrig = agent.stoppingDistance;
+        speedOrig = agent.speed;
     }
 
     // Update is called once per frame
@@ -41,5 +47,34 @@ public class friendlyAI : MonoBehaviour
 
             }
         }
+    }
+    private bool canSeePlayer()
+    {
+        playerDirection = (gameManager.instance.player.transform.position - headPos.position).normalized;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDirection.x, 0, playerDirection.z), transform.forward);
+
+        Debug.Log(angleToPlayer);
+        Debug.DrawRay(headPos.position, playerDirection);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDirection, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewAngle)
+            {
+                agent.stoppingDistance = stoppingDistOrig;
+                agent.SetDestination(gameManager.instance.player.transform.position);
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    // facePlayer();
+                }
+                if (!isTalking && angleToPlayer <= viewAngle)
+                {
+                    // call the methods to talk to the player.
+                }
+                return true;
+            }
+        }
+        agent.stoppingDistance = 0;
+        return false;
     }
 }
