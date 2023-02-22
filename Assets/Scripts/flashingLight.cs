621ba96ac4a510f5057400af61b1028cc0590203
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-22 18:37:46 -0500

    Light script
    Working on lights flashing.

--- Assets/Scripts/flashingLight.cs
+++ Assets/Scripts/flashingLight.cs
@@ -5,15 +5,48 @@ using UnityEngine;
 public class flashingLight : MonoBehaviour
 {
     [SerializeField] GameObject redLights;
+    [SerializeField] GameObject redBulb;
+    bool isFlashing;
+    bool isFirstRound;
+    bool isSecondRound;
+
+    private void Start()
+    {
+        isFirstRound = true;
+        isSecondRound = false;
+        isFlashing = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        redLights.SetActive(false);
-        StartCoroutine(flash());
+        if (isFlashing == false)
+        {
+            isFlashing = true;
+            if (isFirstRound == true)
+            {
+                redLights.SetActive(false);
+                redBulb.SetActive(false);
+                isSecondRound = true;
+                isFirstRound = false;
+            }
+            //StartCoroutine(waitTurnOff());
+            StartCoroutine(flash());
+        }
     }
     IEnumerator flash()
     {
         yield return new WaitForSeconds(3);
-        redLights.SetActive(true);
+        if (isSecondRound == true)
+        {
+            redLights.SetActive(true);
+            redBulb.SetActive(true);
+            isSecondRound = false;
+            isFirstRound = true;
+        }
+        isFlashing = false;
     }
+    //IEnumerator waitTurnOff()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //}
 }
