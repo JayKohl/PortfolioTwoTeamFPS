33605bf3e7839e4e37d6e13c9067d04b3bb05097
MBujosa <mbujosa@student.fullsail.edu>
2023-02-22 14:20:44 -0500

    Revert "UI update"
    This reverts commit d8a78864466e63158868b3d7daf9abf4180bf3b0.

--- Assets/Scripts/shield.cs
+++ Assets/Scripts/shield.cs
@@ -1,23 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class shield : MonoBehaviour
 {
     public int shieldHP = 5;
     [SerializeField] int shieldOrig;
     [SerializeField] float cooldown;
-    Color thisColor = Color.red;
-    Color originalColor = Color.red;
+
     void Start()
     {
-        thisColor.a = 0.20f;
-        originalColor.a = gameManager.instance.shieldUI.GetComponent<Image>().color.a;
-        originalColor.g = gameManager.instance.shieldUI.GetComponent<Image>().color.g;
-        originalColor.b = gameManager.instance.shieldUI.GetComponent<Image>().color.b;
-        originalColor.r = gameManager.instance.shieldUI.GetComponent<Image>().color.r;
-        thisColor.a = 1;
+       
         shieldOrig = shieldHP;
     }
     public void shieldStart()
@@ -28,16 +21,14 @@ public class shield : MonoBehaviour
     public void shieldTakeDamage(int dmg)
     {
         shieldHP -= dmg;
-        StartCoroutine(flashShield());
         if (shieldHP <= 0)
         {
-            
+
             gameManager.instance.shieldUI.SetActive(false);
             //gameManager.instance.shieldCoolDown();
             gameObject.SetActive(false);
             gameManager.instance.shieldOn = false;
             shieldHP = shieldOrig;
-            
 
         }
     }
@@ -52,12 +43,5 @@ public class shield : MonoBehaviour
     public float GetCoolDown()
     {
         return cooldown;
-    }
-
-    IEnumerator flashShield()
-    {
-        gameManager.instance.shieldUI.GetComponent<Image>().color = originalColor;
-        yield return new WaitForSeconds(.1f);
-        gameManager.instance.shieldUI.GetComponent<Image>().color = thisColor;
     }
 }
