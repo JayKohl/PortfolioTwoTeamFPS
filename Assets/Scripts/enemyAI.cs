02d014ccd41719b2a477be386f729d9490002ddc
MBujosa <mbujosa@student.fullsail.edu>
2023-02-22 00:24:39 -0500

    grenades work now
    - turrets had a high radius of 90 and were taking damage from across the map. this is fixed now.

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -11,7 +11,6 @@ public abstract class enemyAI : MonoBehaviour, IDamage
     [SerializeField] Renderer model;
     public NavMeshAgent agent;
     [SerializeField] protected Animator anim;
-    [SerializeField] int grenadeDam;
 
     [Header("----- Enemy Stats -----")]
     [SerializeField] protected float playerYOffset;
@@ -170,10 +169,6 @@ public abstract class enemyAI : MonoBehaviour, IDamage
         if (other.CompareTag("Player"))
         {
             isPlayerInRange = true;
-        }
-        if (other.CompareTag("Grenade"))
-        {
-            takeDamage(grenadeDam);
         }
     }
     public void OnTriggerExit(Collider other)
