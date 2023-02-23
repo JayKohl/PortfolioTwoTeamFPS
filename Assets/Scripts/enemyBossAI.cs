1837bbadfaa7c44c336c253000d133125216a804
JayKohl <106552811+JayKohl@users.noreply.github.com>
2023-02-23 07:20:04 -0500

    Update enemyBossAI.cs
    fixed boss script

--- Assets/Scripts/enemyBossAI.cs
+++ Assets/Scripts/enemyBossAI.cs
@@ -92,9 +92,8 @@ public class enemyBossAI : enemyAI
         {
             gameManager.instance.bossDead = true;
             GetComponent<Collider>().enabled = false;
-            gameManager.instance.endGameTrigger.GetComponent<endGame>().endGameColliderOn();
             GetComponentInChildren<Canvas>().enabled = false;
-            GameObject fuel = Instantiate(fuelCap, gameObject.transform.position + new Vector3 (0, 0, -5), fuelCap.transform.rotation);
+            GameObject fuel = Instantiate(fuelCap, gameObject.transform.position + new Vector3(0, 0, -5), fuelCap.transform.rotation);
             anim.SetBool("Dead", true);
             agent.enabled = false;
             aud.PlayOneShot(audDeath[Random.Range(0, audDeath.Length)], audDeathVol);
