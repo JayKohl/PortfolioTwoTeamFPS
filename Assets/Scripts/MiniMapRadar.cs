377c89d93b11e7a179940fa55479eec555dee395
MBujosa <mbujosa@student.fullsail.edu>
2023-02-23 07:05:47 -0500

    update mapRadar

--- Assets/Scripts/MiniMapRadar.cs
+++ Assets/Scripts/MiniMapRadar.cs
@@ -6,12 +6,15 @@ public class MiniMapRadar : MonoBehaviour
 {
     public Transform player;
 
+    private void Start()
+    {
+        player = gameManager.instance.player.transform;
+    }
     private void LateUpdate()
     {
         Vector3 position = player.position;
         position.y = transform.position.y;
-        transform.position = position;
-        player = gameManager.instance.player.transform;
+        transform.position = position;        
         transform.rotation = Quaternion.Euler(90f, player.localEulerAngles.y, 0);
     }
 }
