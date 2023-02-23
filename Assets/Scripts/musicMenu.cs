f78b74f4dd5d6918bf7a1e73328f6080c05e2af8
MBujosa <mbujosa@student.fullsail.edu>
2023-02-23 08:59:03 -0500

    added music to menus

---
+++ Assets/Scripts/musicMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicMenu : MonoBehaviour
+{
+    [SerializeField] AudioSource aud;
+    [SerializeField] public AudioClip menuAud;
+    [Range(0, 1)] [SerializeField] public float menuVol;
+
+    void Start()
+    {
+        aud.PlayOneShot(menuAud, menuVol);
+    }
+}
