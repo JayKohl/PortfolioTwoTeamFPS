cb61470872c2b78b6b81bff44bc2560b9214fed9
JayKohl <jaymkohl@gmail.com>
2023-03-03 01:17:28 -0500

    Update enemyBugAI.cs

--- Assets/Scripts/enemyBugAI.cs
+++ Assets/Scripts/enemyBugAI.cs
@@ -14,15 +14,142 @@ public class enemyBugAI : enemyAI
 
     System.Random randomAttack;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        hitPointsOrig = hitPoints;
+        startingPos = transform.position;
+        stoppingDistOrig = agent.stoppingDistance;
+        speedOrig = agent.speed;
+        isAgro = false;
+
+        randomAttack = new System.Random();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (agent.isActiveAndEnabled)
+        {
+            anim.SetFloat("Speed", agent.velocity.normalized.magnitude);
+            if (isPlayerInRange)
+            {
+                isAgro = true;
+                if (!canSeePlayer())
+                {
+                    agent.destination = gameManager.instance.player.transform.position;
+                }
+            }
+            else if (agent.destination != gameManager.instance.player.transform.position && isAgro == false)
+            {
+                StartCoroutine(roam());
+            }
+            else if (agent.destination != gameManager.instance.player.transform.position && isAgro == true)
+            {
+                agent.destination = gameManager.instance.player.transform.position;
+            }
+        }
+    }
+    protected override bool canSeePlayer()
+    {
+        playerDirection = (gameManager.instance.player.transform.position - shootPosition.position).normalized;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDirection.x, 0, playerDirection.z), transform.forward);
+
+        Debug.Log(angleToPlayer);
+        Debug.DrawRay(headPos.position, playerDirection);
+
+        Vector3 two = agent.transform.position;
+        Vector3 one = gameManager.instance.player.transform.position;
+        float distanceToEnemy = Mathf.Sqrt(Mathf.Pow((two.x - one.x), 2) + Mathf.Pow((two.y - one.y), 2) + Mathf.Pow((two.z - one.z), 2));
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDirection, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewAngle)
+            {
+                int selectAttack = randomAttack.Next(1, 11);
+
+                agent.stoppingDistance = stoppingDistOrig;
+                agent.speed = speedChase;
+                agent.SetDestination(gameManager.instance.player.transform.position);
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    facePlayer();
+                }
+
+                if (agent.isActiveAndEnabled)
+                {
+                    // basic attacks
+                    if (!isMelee && angleToPlayer <= shootAngle && distanceToEnemy <= 7 && selectAttack <= 4)
+                    {
+                        StartCoroutine(melee());
+                    }
+                    else if (!isMelee && angleToPlayer <= shootAngle && distanceToEnemy <= 7 && selectAttack > 4 && selectAttack <= 8)
+                    {
+                        isMelee = true;
+                        StartCoroutine(meleeTwo());
+                    }
+                    else if (!isMelee && angleToPlayer <= shootAngle && distanceToEnemy <= 7 && selectAttack > 8)
+                    {
+                        isMelee = true;
+                        StartCoroutine(meleeRam());
+                    }
+                    return true;
+                }
+            }
+        }
+        facePlayer();
+        agent.stoppingDistance = stoppingDistOrig;
+        return false;
+    }
+    protected IEnumerator meleeRam()
+    {
+        //isMelee = true;
+        anim.SetTrigger("Ram");
+        yield return new WaitForSeconds(meleeRate);
+        isMelee = false;
+    }
+    public void meleeRamColliderOn()
+    {
+        meleeColliderRam.enabled = true;
+    }
+    public void meleeRamColliderOff()
+    {
+        meleeColliderRam.enabled = false;
+    }
+    protected IEnumerator meleeTwo()
+    {
+        //isMelee = true;
+        anim.SetTrigger("MeleeTwo");
+        yield return new WaitForSeconds(meleeRate);
+        isMelee = false;
+    }
+    public void meleeColliderTwoOn()
+    {
+        meleeColliderTwo.enabled = true;
+    }
+    public void meleeColliderTwoOff()
+    {
+        meleeColliderTwo.enabled = false;
+    }
+    public override void takeDamage(int dmg)
+    {
+        hitPoints -= dmg;
+        if (hitPoints <= 0)
+        {
+
+            GetComponent<Collider>().enabled = false;
+            GetComponentInChildren<Canvas>().enabled = false;
+            aud.PlayOneShot(audDeath[UnityEngine.Random.Range(0, audDeath.Length)], audDeathVol);
+            anim.SetBool("Dead", true);
+            agent.enabled = false;
+        }
+        else
+        {
+            anim.SetTrigger("Damage");
+            aud.PlayOneShot(audTakeDamage[UnityEngine.Random.Range(0, audTakeDamage.Length)], audTakeDamageVol);
+            // melee add a function for turning off the weapon collider.
+            agent.SetDestination(gameManager.instance.player.transform.position);
+            StartCoroutine(flashDamage());
+        }
     }
 }
