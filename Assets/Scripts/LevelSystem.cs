be4930f776598aed4d478a0e767b75d16e0b9ed5
Keith Laurie <KTLaurie@student.fullsail.edu>
2023-03-03 20:01:21 -0500

    Added levelsystem script and updated player prefab

---
+++ Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSystem : MonoBehaviour
+{
+    public int playerLevel;
+    public int LevelMax = 10;
+    public float currentXP;
+    public float NeededXP;
+    public int tokenAmount;
+    [Header("Calculations")]
+    [Range(0.01f, 0.6f)] public float VarX;
+    [Range(1.0f,3.5f)]public float VarY;
+
+    float lerpTimer;
+    float delayTimer;
+
+   // [SerializeField] public Image frontXPbar;
+   // [SerializeField] public Image backXPbar;
+   // [SerializeField] public Image tokenImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //frontXPbar.fillAmount = currentXP / NeededXP;
+        //backXPbar.fillAmount = currentXP / NeededXP;
+        //LevelUp();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //UpdateXPBar();
+        if (Input.GetKeyDown(KeyCode.Equals))
+            GainExperiance(120);
+        if (currentXP > NeededXP)
+            LevelUp();
+    }
+
+    //public void UpdateXPBar()
+    //{
+    //    float xpFraction = currentXP / NeededXP;
+    //    float Fxp = frontXPbar.fillAmount;
+    //    if (Fxp < xpFraction)
+    //    {
+    //        delayTimer += Time.deltaTime;
+    //        backXPbar.fillAmount = xpFraction;
+    //        if (delayTimer > 3)
+    //        {
+    //            lerpTimer += Time.deltaTime;
+    //            float percentComplete = lerpTimer / 4;
+    //            frontXPbar.fillAmount = Mathf.Lerp(Fxp, backXPbar.fillAmount, percentComplete);
+    //        }
+    //    }
+    //}
+
+    public void GainExperiance(float gainedXP)
+    {
+        if (playerLevel < 10)
+        {
+            currentXP += gainedXP;
+        }
+        
+        
+        
+        lerpTimer = 0f;
+        delayTimer = 0f;
+    }
+
+    public void LevelUp()
+    {
+        playerLevel++;
+        //frontXPbar.fillAmount = 0f;
+        //backXPbar.fillAmount = 0f;
+        currentXP = Mathf.RoundToInt(currentXP - NeededXP);
+        tokenAmount += playerLevel;
+        gameManager.instance.playerScript.giveHP(gameManager.instance.playerScript.hpOriginal);
+        NeededXP = CalculateXP();
+    }
+
+    private int CalculateXP()
+    {
+        
+            int requiredxp = 0;
+            return requiredxp = (int)Mathf.Floor(Mathf.Pow((playerLevel / VarX), VarY));
+        
+        
+        
+        
+    }
+}
