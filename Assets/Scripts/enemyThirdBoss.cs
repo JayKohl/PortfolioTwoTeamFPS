6ead707657b23f0d7f73f5a72a75ee704d49623e
JayKohl <jaymkohl@gmail.com>
2023-03-05 14:01:52 -0500

    Level 3 boss update.

--- Assets/Scripts/enemyThirdBoss.cs
+++ Assets/Scripts/enemyThirdBoss.cs
@@ -29,4 +29,82 @@ public class enemyThirdBoss : enemyAI
         isFlip = true;
         topPiece.transform.Rotate(180f, 0f, 0f, Space.Self);
     }
+    public override void takeDamage(int dmg)
+    {
+        hitPoints -= dmg;
+        if (hitPoints <= 0)
+        {
+            GetComponent<Collider>().enabled = false;
+            //GetComponentInChildren<Canvas>().enabled = false;
+            //aud.PlayOneShot(audDeath[Random.Range(0, audDeath.Length)], audDeathVol);
+            //anim.SetBool("Dead", true);
+            agent.enabled = false;
+        }
+        else
+        {
+            anim.SetTrigger("Damage");
+            if (dmg > 0)
+            {
+                aud.PlayOneShot(audTakeDamage[Random.Range(0, audTakeDamage.Length)], audTakeDamageVol);
+            }
+            // melee add a function for turning off the weapon collider.
+            //agent.SetDestination(gameManager.instance.player.transform.position);
+            StartCoroutine(flashDamage());
+        }
+    }
+    protected override bool canSeePlayer()
+    {
+        playerDirection = (gameManager.instance.player.transform.position - headPos.position).normalized;
+        // playerDirection.y += 1;
+        //playerYOffset = playerDirection.y;
+        //playerDirection = gameManager.instance.player.transform.position - transform.position;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDirection.x, 0, playerDirection.z), transform.forward);
+
+        Debug.Log(angleToPlayer);
+        Debug.DrawRay(headPos.position, playerDirection);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDirection, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewAngle)
+            {
+                //agent.stoppingDistance = stoppingDistOrig;
+                //agent.speed = speedChase;
+                //agent.SetDestination(gameManager.instance.player.transform.position);
+                //if (agent.remainingDistance < agent.stoppingDistance)
+                //{
+                //    //facePlayer();
+                //}
+                if (!isShooting) //&& angleToPlayer <= shootAngle)
+                {
+                    StartCoroutine(shoot());
+                }
+                return true;
+            }
+        }
+        //agent.stoppingDistance = 0;
+        return false;
+    }
+    protected override IEnumerator shoot()
+    {
+        isShooting = true;
+        createBullet();
+        //anim.SetTrigger("Shoot");
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+    public override void createBullet()
+    {
+        if (!blind)
+        {
+            GameObject bulletClone = Instantiate(bullet, shootPosition.position, bullet.transform.rotation);
+            bulletClone.GetComponent<Rigidbody>().velocity = playerDirection * bulletSpeed;
+        }
+        else
+        {
+            GameObject bulletClone = Instantiate(bullet, shootPosition.position, bullet.transform.rotation);
+            bulletClone.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(0, 0.5f), Random.Range(0, 0.5f), Random.Range(0, 0.5f)) * bulletSpeed;
+        }
+        //aud.PlayOneShot(audBasicAttack[Random.Range(0, audBasicAttack.Length)], audBasicAttackVol);
+    }
 }
