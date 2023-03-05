4fcd9c7a48738f8933ffb1e9949992e343f9a0d9
JayKohl <jaymkohl@gmail.com>
2023-03-05 13:15:18 -0500

    level 3 boss update

--- Assets/Scripts/enemyThirdBoss.cs
+++ Assets/Scripts/enemyThirdBoss.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyThirdBoss : MonoBehaviour
+public class enemyThirdBoss : enemyAI
 {
     [SerializeField] GameObject topPiece;
-
+    bool isFlip;
     // Start is called before the first frame update
-    //void Start()
-    //{
-    //}
+    void Start()
+    {
+        agentStop();
+        isFlip = false;
+        Vector3 StartingPos = topPiece.transform.rotation.eulerAngles;
+    }
 
     // Update is called once per frame
     void Update()
     {
+
         topPiece.transform.Rotate(0f, 1f, 0f, Space.Self);
+        if (!isFlip)
+        {
+            flipObject();
+        }
+    }
+    private void flipObject()
+    {
+        isFlip = true;
+        topPiece.transform.Rotate(180f, 0f, 0f, Space.Self);
     }
 }
