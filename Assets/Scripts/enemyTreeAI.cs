26dd9ae0a68933058dd9cbc8f893ca4ee29c9edf
JayKohl <jaymkohl@gmail.com>
2023-03-06 18:45:29 -0500

    enemy tree update
    enemy now hides under ground and sprouts when in range.

--- Assets/Scripts/enemyTreeAI.cs
+++ Assets/Scripts/enemyTreeAI.cs
@@ -5,40 +5,58 @@ using UnityEngine;
 public class enemyTreeAI : enemyAI
 {
     int hitPointsOrig;
-    bool firstTime;
+    bool isFirstTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        firstTime = true;
+        //gameObject.SetActive(false);
+        //gameObject.transform.Translate(0f, -7f, 0f, Space.Self);
+        isFirstTime = true;
         hitPointsOrig = hitPoints;
-        agentStop();
-        anim.SetTrigger("Hide");
+        anim.SetTrigger("UnderGround");
+        //agentStop();
+        //anim.SetTrigger("Hide");
     }
 
     // Update is called once per frame
     void Update()
     {
-        anim.SetFloat("IdleSpeed", 1f);
-        if (canSeePlayer())
+        //if (!isPlayerInRange && !isFirstTime)
+        //{
+        //}
+        if (isPlayerInRange == true && isFirstTime == true)
         {
-            //anim.SetBool("IsNotInGround", true);
-            //anim.SetTrigger("Idle");
-            // first attack
+            //gameObject.SetActive(true);
+            isFirstTime = false;
+            anim.SetTrigger("sprout");
+            StartCoroutine(sprout());
         }
-        else if (!canSeePlayer() && isPlayerInRange)
-        {
-            //anim.SetTrigger("Idle");
-            // round attack
-        }
+        //else if (isFirstTime == false)
+        //{
+
+        //    //anim.SetFloat("IdleSpeed", 1f);
+        //    if (canSeePlayer())
+        //    {
+        //        //anim.SetBool("IsNotInGround", true);
+        //        //anim.SetTrigger("Idle");
+        //        // first attack
+        //    }
+        //    else if (!canSeePlayer() && isPlayerInRange)
+        //    {
+        //        //anim.SetTrigger("Idle");
+        //        // round attack
+        //    }
+        //}
+    }
+    IEnumerator sprout()
+    {
+        anim.SetTrigger("OutOfGround");
+        yield return new WaitForSeconds(1.1f);
+
     }
     protected override bool canSeePlayer()
     {
-        if (firstTime)
-        {
-            firstTime = false;
-            anim.SetTrigger("OutOfGround");
-        }
         playerDirection = (gameManager.instance.player.transform.position - headPos.position).normalized;
         angleToPlayer = Vector3.Angle(new Vector3(playerDirection.x, 0, playerDirection.z), transform.forward);
 
