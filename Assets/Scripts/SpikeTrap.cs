869c0babf042cdf87842498969e9cfe92ac4d3d7
MBujosa <mbujosa@student.fullsail.edu>
2023-03-06 20:27:47 -0500

    updated spike trap prefab

--- Assets/Scripts/SpikeTrap.cs
+++ Assets/Scripts/SpikeTrap.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class SpikeTrap : MonoBehaviour
 {
+    [SerializeField] GameObject spikes;
     [SerializeField] AudioSource aud;
-    [SerializeField] AudioClip[] spikeAud;
+    [SerializeField] AudioClip[] spikeAud;    
     [Range(0, 1)] [SerializeField] float audspikeVol;
+    bool playerIn;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !playerIn)
         {
-            gameManager.instance.playerScript.takeDamage(50);
-            aud.PlayOneShot(spikeAud[Random.Range(0, spikeAud.Length)], audspikeVol);
+            playerIn = true;
+            StartCoroutine(spikesUp());
         }
+    }
+    IEnumerator spikesUp()
+    {
+        spikes.transform.position = spikes.transform.position + new Vector3(0, .5f, 0);
+        aud.PlayOneShot(spikeAud[Random.Range(0, spikeAud.Length)], audspikeVol);
+        yield return new WaitForSeconds(.5f);
+        gameManager.instance.playerScript.takeDamage(50);
     }
 }
