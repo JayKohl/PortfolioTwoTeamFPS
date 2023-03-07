d453126fb7d4a4ff3f3452c28e9957d313a5edc0
Teirons <TMIRONS@STUDENT.FULLSAIL.EDU>
2023-03-06 22:49:18 -0500

    Added Audio and destory slots for Breakable Wall

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -8,11 +8,15 @@ public class Breakable : MonoBehaviour, IDamage
     [SerializeField] Renderer model;
     [SerializeField] AudioSource aud;
     [SerializeField] GameObject destroyEffect;
+    [SerializeField] GameObject destroyed;
 
     [Range(1, 25)] [SerializeField] int HP;
-    
+
+    [Header("----- Sound -----")]
     [SerializeField] AudioClip[] audWallBreak;
     [Range(0, 1)] [SerializeField] float audWallBreakVol;
+    [SerializeField] AudioClip[] audWallDie;
+    [Range(0, 1)] [SerializeField] float audWallDieVol;
 
     public void takeDamage(int dmg)
     {
@@ -20,8 +24,9 @@ public class Breakable : MonoBehaviour, IDamage
 
         if (HP <= 0)
         {
-            //aud.PlayOneShot(audWallBreak[Random.Range(0, audWallBreak.Length)], audWallBreakVol);
+            aud.PlayOneShot(audWallDie[Random.Range(0, audWallDie.Length)], audWallDieVol);
             //Instantiate(destroyEffect, transform.position, destroyEffect.transform.rotation);
+            //Instantiate(destroyed, transform.position, destroyed.transform.rotation);
             Destroy(gameObject);
         }
         else
@@ -31,6 +36,7 @@ public class Breakable : MonoBehaviour, IDamage
     }
     IEnumerator flashBreakDamage()
     {
+        aud.PlayOneShot(audWallBreak[Random.Range(0, audWallBreak.Length)], audWallBreakVol);
         model.material.color = Color.gray;
         yield return new WaitForSeconds(0.15f);
         model.material.color = Color.white;
