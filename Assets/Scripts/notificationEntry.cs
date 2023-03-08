68921dc23208bbed239799b6b0c0ecdfd293f47a
MBujosa <mbujosa@student.fullsail.edu>
2023-03-08 14:11:58 -0500

    updated notifications

---
+++ Assets/Scripts/notificationEntry.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class notificationEntry : MonoBehaviour
+{
+    [SerializeField] notifications texture;
+    bool playerIn;
+    private void Start()
+    {
+        playerIn = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !playerIn)
+        {
+            playerIn = true;
+            StartCoroutine(gameManager.instance.notificationDisplay(texture));
+            Destroy(gameObject);
+        }
+    }
+}
