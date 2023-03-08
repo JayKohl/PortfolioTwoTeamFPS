801888cbaa310d41f89616d5839ce0afdebf8438
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2023-03-08 01:20:01 -0500

    Added code for compas

--- Assets/Scripts/Objectivepoint.cs
+++ Assets/Scripts/Objectivepoint.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Objectivepoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Image locator;
+    public Transform location;
     void Update()
     {
-        
+        locator.transform.position = Camera.main.WorldToScreenPoint(location.position);
     }
 }
