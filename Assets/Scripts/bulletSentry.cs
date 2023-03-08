c4e62145b1fd699b9774fc091929efcd7b9a32ee
MBujosa <mbujosa@student.fullsail.edu>
2023-03-07 23:24:59 -0500

    working on sentry gun ability

---
+++ Assets/Scripts/bulletSentry.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletSentry : MonoBehaviour
+{
+    [Header("----- Bullet Info -----")]
+    public int bulletDamage;
+    [SerializeField] int maxTravelTime;
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, maxTravelTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        target = other.gameObject;        
+        if (other.CompareTag("Enemy"))
+        {
+            target.GetComponent<enemyAI>().takeDamage(bulletDamage);
+        }
+        Destroy(gameObject);
+    }
+}
