f2b75877cd8d23617e423a1646bfdb57b319599b
Teirons <TMIRONS@STUDENT.FULLSAIL.EDU>
2023-03-08 16:53:13 -0500

    Updated Spike/Wind-Tunnel prefab

--- Assets/Scripts/SpikeTrap.cs
+++ Assets/Scripts/SpikeTrap.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class SpikeTrap : MonoBehaviour
 {
     [SerializeField] GameObject spikes;
+
+    [Header("----- Sound -----")]
     [SerializeField] AudioSource aud;
     [SerializeField] AudioClip[] spikeAud;
     [Range(0, 1)] [SerializeField] float audspikeVol;
+
+    //[Header("----- Timer -----")]
+    //[Range(0, 5)] [SerializeField] float spikeTimer;
+
+    //[Header("----- Damage -----")]
+    //[Range(0, 100)] [SerializeField] int spikeDamage;
+
     bool playerIn;
 
     private void OnTriggerEnter(Collider other)
@@ -15,16 +24,17 @@ public class SpikeTrap : MonoBehaviour
         if (other.CompareTag("Player") && !playerIn)
         {
             playerIn = true;
-            spikes.transform.position = spikes.transform.position + new Vector3(0, .5f, 0);
+            spikes.transform.position = spikes.transform.position + new Vector3(0, 1f, 0);
             StartCoroutine(spikesUp());
         }
     }
+
     IEnumerator spikesUp()
     {
-        aud.PlayOneShot(spikeAud[Random.Range(0, spikeAud.Length)], audspikeVol);
         yield return new WaitForSeconds(.5f);
+        aud.PlayOneShot(spikeAud[Random.Range(0, spikeAud.Length)], audspikeVol);
         playerIn = false;
-        spikes.transform.position = spikes.transform.position + new Vector3(0, -.5f, 0);
-        gameManager.instance.playerScript.takeDamage(50);
+        spikes.transform.position = spikes.transform.position + new Vector3(0, -1f, 0);
+        gameManager.instance.playerScript.takeDamage(100);
     }
 }
