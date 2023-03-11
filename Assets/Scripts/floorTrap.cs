ebc9307f4bc218bb2ae757eb4b3b03355264b02f
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2023-03-11 05:22:32 -0500

    scriptable object and trapScript

---
+++ Assets/Scripts/floorTrap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class floorTrap : ScriptableObject
+{
+	public ParticleSystem trapVisual;
+	public AudioClip soundEffect;
+	public int activeTime;
+	public int effectTime;
+	public int damage;
+	bool trapActive;
+
+	//Effecrt Type
+	// 1 = poison
+	// 2 = electrecuted
+	// 3 = burning
+	// 4 = slow
+	public int effectType;
+}
---
+++ Assets/Scripts/floorTrapActivation.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floorTrapActivation : MonoBehaviour
+{
+
+	[SerializeField] floorTrap trapsType;
+
+	ParticleSystem trapVisual;
+    AudioClip soundEffect;
+	int activeTime;
+	int effectTime;
+	int damage;
+	bool trapActive;
+
+	//Effecrt Type
+	// 1 = poison
+	// 2 = electrecuted
+	// 3 = burning
+	// 4 = slow
+	int effectType;
+    private void Start()
+    {
+		trapVisual = trapsType.trapVisual;
+		soundEffect = trapsType.soundEffect;
+		activeTime = trapsType.activeTime;
+		effectTime = trapsType.effectTime;
+		damage = trapsType.damage;
+		trapActive = trapsType.trapActive;
+		effectType = trapsType.effectType;
+	}
+    void Update()
+	{
+
+		StartCoroutine(TrapCycle());
+
+	}
+
+	void ontrigerEnter(GameObject other)
+	{
+		if (other.CompareTag("player") && trapActive)
+		{
+
+			switch(effectType)
+			{
+			 case(1):
+				gameManager.instance.playerScript.Poisoned(effectTime, damage, soundEffect);
+					break;
+				case (2):
+			    gameManager.instance.playerScript.Electrecuted(effectTime, damage, soundEffect);
+					break;
+				case (3):
+			    gameManager.instance.playerScript.Burning(effectTime, damage, soundEffect);
+					break;
+				case (4):
+			    StartCoroutine(gameManager.instance.playerScript.Slowed(effectTime, damage));
+					break;
+				default:
+				 break;
+			 
+
+			}
+
+		}
+	}
+
+	IEnumerator TrapCycle()
+	{
+		trapActive = true;
+		Instantiate(trapVisual);
+		yield return new WaitForSeconds(activeTime);
+		trapActive = false;
+		Destroy(trapVisual);
+	}
+
+
+
+}
