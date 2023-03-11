a9ea478028ed89a0f3811e219df53263b33f636c
MBujosa <mbujosa@student.fullsail.edu>
2023-03-10 23:28:57 -0500

    update weapon stats script

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -47,7 +47,6 @@ public class playerController : MonoBehaviour
     [SerializeField] GameObject gravityBomb;
     [SerializeField] GameObject sentryGun;
     [SerializeField] string weaponName;
-    //[SerializeField] AudioClip weaponAudio;
     [SerializeField] public List<AudioClip> weaponAudio = new List<AudioClip>();
     [Range(0, 1)] [SerializeField] float weaponAudioVol;
     public MeshRenderer visible;
@@ -296,7 +295,6 @@ public class playerController : MonoBehaviour
 
     IEnumerator shoot()
     {
-        //aud.PlayOneShot(weaponAudio, weaponAudioVol);
         aud.PlayOneShot(weaponAudio[Random.Range(0, weaponAudio.Count)], weaponAudioVol);
         isShooting = true;
         StartCoroutine(gunShootFlash());
