2b6a97d5b36f57e4d60471e080dae779cbbf0004
Keith Laurie <KTLaurie@student.fullsail.edu>
2023-03-12 16:28:05 -0400

    Messages are randomizing

--- Assets/Scripts/ModifyText.cs
+++ Assets/Scripts/ModifyText.cs
@@ -8,17 +8,60 @@ public class ModifyText : MonoBehaviour
 
     public TMP_Text hintText;
     public string hint1 = "This is a hint.";
+    public string hint2 = "This is a hint as well, how are you today?";
     public List<string> allHints;
     public List<string> usedHints;
-
+    public string selectedHint;
+    public bool hintUp;
+    
     void Start()
     {
-        hintText.text = hint1;
+        allHints.Add(hint1);
+        allHints.Add(hint2);
     }
 
-    
+
     void Update()
     {
-        
+        if (gameManager.instance.isPaused)
+        {
+            if (!hintUp)
+            {
+                hintUp = true;
+                HintSelect();
+            }   
+        }
+        else
+        {
+            hintUp = false;
+        }
+    }
+
+    public void HintSelect()
+    {
+        if (allHints.Count <= 0)
+        {
+            ResetHintLists();
+        }
+
+        int position;
+        position = Random.Range(0, allHints.Count);
+        selectedHint = allHints[position];
+        hintText.text = selectedHint;
+        allHints.RemoveAt(position);
+        usedHints.Add(selectedHint);
+
+    }
+
+    private void ResetHintLists()
+    {
+        string temp;
+
+        for (int i = 0; i < usedHints.Count; i++)
+        {
+            temp = usedHints[i];
+            allHints.Add(temp);
+        }
+        usedHints.Clear();
     }
 }
