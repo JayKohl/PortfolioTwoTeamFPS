e8e71b299201f2d01f28d5d836afbe70cafc14e1
Keith Laurie <KTLaurie@student.fullsail.edu>
2023-03-13 18:56:12 -0400

    Added to audio menu script

--- Assets/Scripts/AudioMenu.cs
+++ Assets/Scripts/AudioMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,30 @@ public class AudioMenu : MonoBehaviour
     [SerializeField] Slider musicSlider;
     [SerializeField] Slider sfxSlider;
     [SerializeField] Slider masterSlider;
+    [SerializeField] float multiplier = 20.0f;
+
+    private void Awake()
+    {
+        musicSlider.onValueChanged.AddListener(HandleMusicSliderValueChanged);
+        sfxSlider.onValueChanged.AddListener(HandleSFXSliderValueChanged);
+        masterSlider.onValueChanged.AddListener(HandleMasterSliderValueChanged);
+    }
+
+    private void HandleMusicSliderValueChanged(float value)
+    {
+        _mixer.SetFloat(musicVolume, value: Mathf.Log10(value) * multiplier);
+    }
+
+    private void HandleSFXSliderValueChanged(float value)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleMasterSliderValueChanged(float value)
+    {
+        throw new NotImplementedException();
+    }
+
 
     // Start is called before the first frame update
     void Start()
