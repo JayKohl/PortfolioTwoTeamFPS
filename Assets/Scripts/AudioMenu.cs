647f888ebe6ae913bd65e01a6f74d3a8cf08c84e
Keith Laurie <KTLaurie@student.fullsail.edu>
2023-03-13 18:30:41 -0400

    Changed audio menu

--- Assets/Scripts/AudioMenu.cs
+++ Assets/Scripts/AudioMenu.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioMenu : MonoBehaviour
 {
-
-
+    [SerializeField] string masterVolume = "MasterVolume";
+    [SerializeField] string musicVolume = "MusicVolume";
+    [SerializeField] string sfxVolume = "SFXVolume";
+    [SerializeField] AudioMixer _mixer;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
 
     // Start is called before the first frame update
     void Start()
