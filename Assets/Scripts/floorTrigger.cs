a81ca54dfe9abe21ab4e33d7e93d4863732353a5
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2023-03-13 08:01:12 -0400

    Added new traps

--- Assets/Scripts/floorTrigger.cs
+++ Assets/Scripts/floorTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class floorTrigger : MonoBehaviour
 {
 	floorTrapActivation trapParent;
+	bool playerIn;
 
     private void Start()
     {
@@ -12,40 +13,55 @@ public class floorTrigger : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
 	{
-
-
-		if (other.CompareTag("Player") && trapParent.trapActive)
-		{
-
-			switch (trapParent.effectType)
+		if(!playerIn)
+        {
+			if (other.CompareTag("Player") && trapParent.trapActive)
 			{
-				case (1):
-					if (gameManager.instance.playerScript.poisoned == false)
-					{
-						gameManager.instance.playerScript.poisoned = true;
-						StartCoroutine(gameManager.instance.playerScript.Poisoned(trapParent.effectTime, trapParent.damage, trapParent.soundEffect));
-					}
 
-					break;
-				case (2):
-					if (gameManager.instance.playerScript.electrecuted == false)
-					{
-						gameManager.instance.playerScript.electrecuted = true;
-						StartCoroutine(gameManager.instance.playerScript.Electrecuted(trapParent.effectTime, trapParent.damage, trapParent.soundEffect));
-					}
-					break;
-				case (3):
-					gameManager.instance.playerScript.Burning(trapParent.effectTime, trapParent.damage, trapParent.soundEffect);
-					break;
-				case (4):
-					StartCoroutine(gameManager.instance.playerScript.Slowed(trapParent.effectTime, trapParent.damage));
-					break;
-				default:
-					break;
+				switch (trapParent.effectType)
+				{
+					case (1):
+						if (gameManager.instance.playerScript.poisoned == false)
+						{
+							gameManager.instance.playerScript.poisoned = true;
+							StartCoroutine(gameManager.instance.playerScript.Poisoned(trapParent.effectTime, trapParent.damage, trapParent.soundEffect));
+						}
+
+						break;
+					case (2):
+						if (gameManager.instance.playerScript.electrecuted == false)
+						{
+							gameManager.instance.playerScript.electrecuted = true;
+							StartCoroutine(gameManager.instance.playerScript.Electrecuted(trapParent.effectTime, trapParent.damage, trapParent.soundEffect));
+						}
+						break;
+					case (3):
+
+						if (gameManager.instance.playerScript.burning == false)
+						{
+							gameManager.instance.playerScript.burning = true;
+							StartCoroutine(gameManager.instance.playerScript.Burning(trapParent.effectTime, trapParent.damage, trapParent.soundEffect));
+						}
+	
+						break;
+					case (4):
+						if (gameManager.instance.playerScript.slowed == false)
+						{
+							gameManager.instance.playerScript.slowed = true;
+							StartCoroutine(gameManager.instance.playerScript.Slowed(trapParent.effectTime, trapParent.damage));
+						}
+						
+						break;
+					default:
+						break;
+				}
+				playerIn = true;
 			}
-
-
 		}
 
 	}
+	private void OnTriggerExit(Collider other)
+    {
+		playerIn = false;
+    }
 }
