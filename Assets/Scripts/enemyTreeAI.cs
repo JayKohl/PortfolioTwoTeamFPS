cd920495d697fdb2999bad6096728ad3231b5c69
MBujosa <mbujosa@student.fullsail.edu>
2023-03-13 03:58:11 -0400

    removed debug logs

--- Assets/Scripts/enemyTreeAI.cs
+++ Assets/Scripts/enemyTreeAI.cs
@@ -58,7 +58,7 @@ public class enemyTreeAI : enemyAI
         playerDirection = (gameManager.instance.player.transform.position - headPos.position).normalized;
         angleToPlayer = Vector3.Angle(new Vector3(playerDirection.x, 0, playerDirection.z), transform.forward);
 
-        Debug.Log(angleToPlayer);
+        //Debug.Log(angleToPlayer);
         Debug.DrawRay(headPos.position, playerDirection);
 
         RaycastHit hit;
