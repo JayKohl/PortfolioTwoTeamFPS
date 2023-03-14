014251680187c6c6ca7cdadb5d62f7235e88085a
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2023-03-13 21:24:40 -0400

    particles for floor trap

--- Assets/Scripts/floorTrapActivation.cs
+++ Assets/Scripts/floorTrapActivation.cs
@@ -7,7 +7,7 @@ public class floorTrapActivation : MonoBehaviour
 
 	[SerializeField] floorTrap trapsType;
 
-	public ParticleSystem trapVisual;
+	[SerializeField]ParticleSystem trapVisual;
 	public AudioClip soundEffect;
 	public int activeTime;
 	public int effectTime;
@@ -28,7 +28,7 @@ public class floorTrapActivation : MonoBehaviour
 
     private void Start()
     {
-		trapVisual = trapsType.trapVisual;
+		
 		soundEffect = trapsType.soundEffect;
 		activeTime = trapsType.activeTime;
 		effectTime = trapsType.effectTime;
@@ -36,9 +36,9 @@ public class floorTrapActivation : MonoBehaviour
 		trapActive = trapsType.trapActive;
 		effectType = trapsType.effectType;
 
-
-		//trapVisual = Instantiate(trapsType.trapVisual, transform.position, transform.rotation);
-		//trapVisual.Stop();
+		
+		
+		
 
 	}
     void Update()
@@ -54,14 +54,14 @@ public class floorTrapActivation : MonoBehaviour
 		yield return new WaitForSeconds(5);
 		
 		trapActive = true;
-		//trapVisual.Play();
-        transform.GetComponent<Renderer>().material.color = Color.red;
+		
+		transform.GetComponent<Renderer>().material.color = Color.red;
 		triggerObject.transform.position = Vector3.Lerp(triggerObject.transform.position, triggerObject2.transform.position, 1);
        
         yield return new WaitForSeconds(5);
 		
         transform.GetComponent<Renderer>().material.color = Color.blue;
-		//trapVisual.Stop();
+		
 		triggerObject.transform.position = Vector3.Lerp(triggerObject.transform.position, triggerObject1.transform.position, 1);
 		trapActive = false;
 		
