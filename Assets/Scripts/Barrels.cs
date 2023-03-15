33630e915b6cba0e29d609fc7dfebdd6bdbfc42c
Angel Joel Garcia Mordan <103035057+OznarG@users.noreply.github.com>
2023-03-15 00:45:01 -0400

    final code for barrels

--- Assets/Scripts/Barrels.cs
+++ Assets/Scripts/Barrels.cs
@@ -6,23 +6,22 @@ public class Barrels : MonoBehaviour, IDamage
 {
     [SerializeField] int barrilHP;
     [SerializeField] GameObject explotion;
-    public int damage;
-    public bool alive;
+
     // Start is called before the first frame update
 
     private void Start()
     {
-        alive = true;
+     
         barrilHP = 20;
     }
     public virtual void takeDamage(int damage)
     {
         barrilHP -= damage;
 
-        if(barrilHP <= 0 && alive)
+        if(barrilHP <= 0)
         {
             Instantiate(explotion, transform.position, transform.rotation);
-            alive = false;
+          
             Destroy(gameObject);
 
         }
