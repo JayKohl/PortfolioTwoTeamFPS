563984f690ba4b12c080acbe915c987132641873
MBujosa <mbujosa@student.fullsail.edu>
2023-03-21 00:39:23 -0400

    updated swarm ability
    swarm ability no longer plays continuously when the game is paused

--- Assets/Scripts/swarm.cs
+++ Assets/Scripts/swarm.cs
@@ -5,8 +5,28 @@ using UnityEngine;
 public class swarm : MonoBehaviour
 {
     [SerializeField] float time = 5;
+    [SerializeField] AudioSource aud;
+    bool isTrue;
+
+    private void Update()
+    {
+        if(gameManager.instance.isPaused)
+        {
+            isTrue = false;
+            aud.Stop();
+        }
+        else if(!isTrue)
+        {
+            isTrue = true;
+            aud.loop = true;
+            aud.Play();
+        }
+    }
+
     private void Start()
     {
+        aud.loop = true;
+        aud.Play();
         StartCoroutine(timerDeath());
     }
     IEnumerator timerDeath()
